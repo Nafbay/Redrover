b0fe76722da1259aa20c9e9aaeb79d779af9b657
Nafbay <baygnafisa@yandex.ru>
2024-10-08 18:22:18 +0300

    add another print

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,6 @@
-print ("Hello world")
+print ("Hello world") # функция вывода
+print('Hello', "World") #
+
+message = "Hello" #вывод с помощью переменной
+print(message)  # переменная - это ссылка на какой-то объект в pycharm
+
